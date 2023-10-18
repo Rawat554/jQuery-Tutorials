581d4eb96f922cdea09f19437bec8da1cbcf0154
prakash <prakashrwt.official@gmail.com>
2023-10-18 19:36:46 +0530

    initial commit

---
+++ jQuery-Tutorials/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace jQuery_Tutorials.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
